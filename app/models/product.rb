9e168b0fe77eec6a9de36301b5a23cb7bb2e4b0b
OguzBayat <oguzbayat77@gmail.com>
2020-03-30 23:45:07 +0300

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
 	has_many :orders
   	has_many :customers, through: :orders
-
   	validates_presence_of :name
 	validates_presence_of :price
 	validates_presence_of :count
